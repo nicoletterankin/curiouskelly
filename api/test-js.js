885c9eac98647adfd30970e10bfe987d53bdec5c
nicoletterankin <nicoletterankin@gmail.com>
2025-12-21 12:41:34 -0800

    Fix API routes: run as CommonJS on Vercel

--- api/test-js.js
+++ api/test-js.js
@@ -2,12 +2,11 @@
  * JavaScript-only test endpoint
  */
 
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({ ok: true, type: 'js', ts: Date.now() });
 };
-
 
 
 
