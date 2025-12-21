4e9a1c1e131dd30fc1d005f7e0044f9cc1c94e88
nicoletterankin <nicoletterankin@gmail.com>
2025-12-21 12:37:36 -0800

    Fix API routes: convert CommonJS to ESM exports

--- api/test-js.js
+++ api/test-js.js
@@ -2,11 +2,12 @@
  * JavaScript-only test endpoint
  */
 
-module.exports = function handler(req, res) {
+export default function handler(req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({ ok: true, type: 'js', ts: Date.now() });
 };
+
 
 
 
