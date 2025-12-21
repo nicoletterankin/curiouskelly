3b682e97f757ccbd027be8e7b07a5bbc817c513c
nicoletterankin <nicoletterankin@gmail.com>
2025-12-21 15:44:16 -0800

    Fix Vercel API 500: align ESM config and outputs

--- api/test-js.js
+++ api/test-js.js
@@ -2,11 +2,11 @@
  * JavaScript-only test endpoint
  */
 
-module.exports = function handler(req, res) {
+export default function handler(req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({ ok: true, type: 'js', ts: Date.now() });
-};
+}
 
 
 
